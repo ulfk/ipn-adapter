760a972957d24a57a3044b8b6224611f3550b6b5
Ulf Kühnle <ulf.kuehnle@gmx.de>
2025-08-15 09:06:08 +0200

    added wordpress-integrated logviewer

---
+++ plugin-main.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package IPN-Adapter
+ */
+/*
+Plugin Name: IPN-Adapter
+Plugin URI: https://github.com/ulfk/ipn-adapter
+Description: IPN Adapter by Ulf Kuehnle.
+Version: 0.1.2
+Author: Ulf Kuehnle
+Author URI: https://ulf-kuehnle.de/
+License: free
+*/
+
+include_once "log-viewer.php";
+
+$wp_plugin_log_viewer = new WP_Plugin_Log_Viewer('ipn-adapter');
+
+
+?>
